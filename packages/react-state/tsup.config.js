5bbaed2ee110a98b339132fb64d68246fa1d93ca
devtor-who <dinnertime.dev@gmail.com>
2024-07-24 01:00:44 +0900

    bundle 사이즈 축소 (cjs삭제, env.d.ts삭제

--- packages/react-state/tsup.config.js
+++ packages/react-state/tsup.config.js
@@ -1,9 +1,8 @@
 // @ts-check
 
 import { defineConfig } from 'tsup';
-import { legacyConfig, modernConfig } from '../../scripts/tsup-config.js';
+import { modernConfig } from '../../scripts/tsup-config.js';
 
 export default defineConfig([
   modernConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
-  legacyConfig({ entry: ['src/**/*.ts', 'src/**/*.tsx'] }),
 ]);
