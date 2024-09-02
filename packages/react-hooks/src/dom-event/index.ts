4acf89a9f6250e95d031f34e70beb14d701c7d59
dinnertime <dinnertime.dev@gmail.com>
2024-09-02 16:41:28 +0900

    dom event hook 추가

---
+++ packages/react-hooks/src/dom-event/index.ts
@@ -0,0 +1,66 @@
+import React from 'react';
+import { type Observable, type OperatorFunction, fromEvent } from 'rxjs';
+import { debounceTime, take, throttleTime } from 'rxjs/operators';
+
+type EventOption =
+  | ({ once?: boolean } & { debounce?: never; throttle?: never })
+  | ({ once?: boolean } & { debounce?: number; throttle?: never })
+  | ({ once?: boolean } & { throttle?: number; debounce?: never });
+
+const buildOperators = (option?: EventOption) => {
+  const pipeOperators = [] as unknown as [OperatorFunction<any, any>];
+  if (option?.debounce !== undefined) {
+    pipeOperators.push(debounceTime(option.debounce));
+  }
+  if (option?.throttle !== undefined) {
+    pipeOperators.push(throttleTime(option.throttle));
+  }
+  if (option?.once) {
+    pipeOperators.push(take(1));
+  }
+
+  return pipeOperators;
+};
+
+const useEvent = <El extends HTMLElement>(
+  target: El | null,
+  eventName: keyof HTMLElementEventMap,
+  callback: (e: Event) => void,
+  option?: EventOption,
+) => {
+  const eventCallback = React.useCallback(callback, [callback]);
+
+  React.useEffect(() => {
+    if (!target) return;
+
+    const pipeOperators = buildOperators(option);
+
+    const event$: Observable<Event> = fromEvent(target, eventName);
+    const eventExecuted$ = event$.pipe(...pipeOperators);
+    const subscription = eventExecuted$.subscribe((e) => {
+      eventCallback(e);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [eventCallback, eventName, option]);
+};
+
+export const useDomEvent = <El extends HTMLElement>(
+  eventName: keyof HTMLElementEventMap,
+  callback: (e: Event) => void,
+  option?: EventOption,
+) => {
+  const ref = React.useRef<El>(null);
+  useEvent(ref.current, eventName, callback, option);
+  return ref;
+};
+
+export const useDocumentEvent = (
+  eventName: keyof HTMLElementEventMap,
+  callback: (e: Event) => void,
+  option?: EventOption,
+) => {
+  useEvent(document.documentElement, eventName, callback, option);
+};
